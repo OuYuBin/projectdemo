984a23f564402c694435f4f360b88631040b3119
yuqiangjia <yuqiangjia@hengtiansoft.com>
2016-08-10 10:40:41 +0800

    珍重重新提交

--- src/main/java/base/map/MapTest.java
+++ src/main/java/base/map/MapTest.java
@@ -1,4 +1,4 @@
-package base.collection;
+package base.map;
 
 import java.util.HashMap;
 import java.util.Hashtable;
@@ -8,8 +8,22 @@ public class MapTest {
 	public static void main(String[] args) {
 		Map<String, String> map = new HashMap<String, String>(7);
 		map.put("1", "String");
+		map.put("1", "String1");
+		map.put(null, "niha");
 		System.out.println(map.get("1"));
+		System.out.println(map.get(0));
+		System.out.println(map.get(null));
 
 		Map<String, String> map2 = new Hashtable<>();
+		// map2.put(null, "ah");
+
+		System.out.println(map2.hashCode());
+
+		Integer a = 10;
+		System.out.println(a.hashCode());
+
+		String ss = "shi";
+		System.out.println(ss.hashCode());
+
 	}
 }
